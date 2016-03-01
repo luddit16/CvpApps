b23caedbfb88a08a3ee3ea527c6221dca85abee1
Kaushik Rajbongshi <kaushikrajbongshi@gmail.com>
2016-03-01 13:44:21 +0530

    Added password hashed mechanism for secure store

--- src/com/cvpapps/controller/LoginController.java
+++ src/com/cvpapps/controller/LoginController.java
@@ -3,6 +3,9 @@
  */
 package com.cvpapps.controller;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cvpapps.form.LoginForm;
+import com.cvpapps.util.PBKDF2Hash;
 
 /**
  * @author cvpapps.com
@@ -31,9 +35,17 @@ public class LoginController {
 			HttpServletRequest request, Model model) {
 		
 		System.out.println("I am in controller");
-		
 		System.out.println("The Email data is:"+loginForm.getEmail());
+		System.out.println("Original password is "+loginForm.getPassword());
 		
+		String hashedPassword = null;
+		try {
+			hashedPassword=PBKDF2Hash.generateStorngPasswordHash(loginForm.getPassword());
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Encrypted password is "+hashedPassword);
  		return new ModelAndView("dashboard");
 	}
 }
